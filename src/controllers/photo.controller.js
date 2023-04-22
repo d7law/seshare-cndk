7c0fc380c8d1c3c392253120faba4f1f98120789
caonhan_bitelle <nhan.cao@bitelle.com>
2023-04-22 09:03:05 +0700

    get homepage posts

--- src/controllers/photo.controller.js
+++ src/controllers/photo.controller.js
@@ -5,8 +5,22 @@ const _ = require("lodash");
 var response = require("../models/ResponseModel").response;
 
 class PhotoController {
+  // Get Home-Page-Post
+  homePagePosts = async (req, res) => {
+    const listPhoto = await Photo.find({ privacy: "public" });
+    if (!listPhoto || listPhoto.length < 1)
+      return res.status(404).json(response(false, listPhoto));
+    return res.status(200).json(response(true, listPhoto));
+  };
+  //Get all Photo of user
+  getPhoto = async (req, res) => {
+    const userId = req.body.userId;
+
+    const photos = await Photo.find({ user_id: userId });
+    res.status(200).json(response(true, photos));
+  };
   // Create New Post/Photo
-  async createPhoto(req, res) {
+  createPhoto = async (req, res) => {
     const userId = req.body.userId;
     const caption = req.body.caption;
     const isAvatar = req.body.isAvatar;
@@ -39,14 +53,6 @@ class PhotoController {
     } catch (err) {
       res.status(502).json(response(false));
     }
-  }
-
-  //Get all Photo of user
-  getPhoto = async (req, res) => {
-    const userId = req.body.userId;
-
-    const photos = await Photo.find({ user_id: userId });
-    res.status(200).json(response(true, photos));
   };
 }
 
--- src/routes/photo.route.js
+++ src/routes/photo.route.js
@@ -2,7 +2,8 @@ const photoRoute = require("express").Router();
 const photoController = require("../controllers/photo.controller");
 const { upload } = require("../services/upload.service");
 
-photoRoute.post("/photo/get-all", photoController.getPhoto);
+photoRoute.post("/photo/homepage-posts", photoController.homePagePosts);
+photoRoute.post("/photo/get-photo-user", photoController.getPhoto);
 photoRoute.post(
   "/photo/upload-photos",
   upload.single("photo"),
